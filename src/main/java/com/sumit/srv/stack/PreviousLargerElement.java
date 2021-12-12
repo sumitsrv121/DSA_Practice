0e8e533e97d11d17c3b3274feaa697f7e7b9168e
sumit <sumitsaurav121@gmail.com>
2021-12-12 11:06:13 +0530

    Larger element to the left of element in array

---
+++ src/main/java/com/sumit/srv/stack/PreviousLargerElement.java
@@ -0,0 +1,25 @@
+package com.sumit.srv.stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class PreviousLargerElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 0, 0, 1, 2, 2, 4};
+        int[] ans = new int[arr.length];
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.empty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if (stack.empty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
