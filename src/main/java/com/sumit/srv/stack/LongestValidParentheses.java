1e3d10c235c8680e95e9997ac8dd0dad9c2ee4cd
sumit <sumitsaurav121@gmail.com>
2021-12-13 20:20:15 +0530

    Longest valid parentheses

--- src/main/java/com/sumit/srv/stack/LongestValidParentheses.java
+++ src/main/java/com/sumit/srv/stack/LongestValidParentheses.java
@@ -3,39 +3,31 @@ package com.sumit.srv.stack;
 import java.util.Stack;
 
 public class LongestValidParentheses {
-    private static final Stack<Character> stack = new Stack<>();
-    private static final Stack<Integer> indexStack = new Stack<>();
-    private static int count = 0;
-
     public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> indexStack = new Stack<>();
+
+        int count = 0;
         indexStack.push(-1);
-        String str = "())";
+        String str = ")()())()";
 
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             if (c == '(') {
                 // push
-                push(c, i);
+                stack.push(i);
+                indexStack.push(i);
             } else if (c == ')') {
                 // pop
-                pop(i);
+                if (stack.empty()) {
+                    indexStack.push(i);
+                } else {
+                    stack.pop();
+                    indexStack.pop();
+                    count = Math.max(count, i - indexStack.peek());
+                }
             }
         }
         System.out.println(count);
-    }
-
-    private static void pop(int index) {
-        if (stack.empty()) {
-            indexStack.push(index);
-        } else {
-            stack.pop();
-            indexStack.pop();
-            count = Math.max(count, index - indexStack.peek());
-        }
-    }
-
-    private static void push(char c, int index) {
-        stack.push(c);
-        indexStack.push(index);
     }
 }
