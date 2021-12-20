d4bd27a9e6ec5b41b429f824c07371afe4858d3f
sumit <sumitsaurav121@gmail.com>
2021-12-20 06:11:56 +0530

    Knapsack 0/1 recursive solution

---
+++ src/main/java/com/sumit/srv/dynamicProgramming/Knapsack01.java
@@ -0,0 +1,26 @@
+package com.sumit.srv.dynamicProgramming;
+
+public class Knapsack01 {
+    public static void main(String[] args) {
+        int[] wt = {1, 3, 4, 5};
+        int[] val = {1, 4, 5, 7};
+        int capacity = 7;
+
+        int maxProfit = findMaximumProfit(wt, val, capacity, val.length);
+        System.out.println(maxProfit);
+
+    }
+
+    private static int findMaximumProfit(int[] wt, int[] val, int capacity, int n) {
+        if (capacity == 0 || n == 0) {
+            return 0;
+        }
+
+        if (wt[n - 1] > capacity) {
+            return findMaximumProfit(wt, val, capacity, n - 1);
+        } else {
+            return Math.max(val[n - 1] + findMaximumProfit(wt, val, capacity - wt[n - 1], n - 1),
+                    findMaximumProfit(wt, val, capacity, n - 1));
+        }
+    }
+}
