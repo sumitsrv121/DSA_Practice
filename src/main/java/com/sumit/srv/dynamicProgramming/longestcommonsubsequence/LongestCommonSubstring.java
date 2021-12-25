8e07d087ba4a369ec440fe4d18395a15bc3d70ec
sumit <sumitsaurav121@gmail.com>
2021-12-25 17:39:26 +0530

    Longest common substring length

--- src/main/java/com/sumit/srv/dynamicProgramming/longestcommonsubsequence/LongestCommonSubstring.java
+++ src/main/java/com/sumit/srv/dynamicProgramming/longestcommonsubsequence/LongestCommonSubstring.java
@@ -1,13 +1,14 @@
 package com.sumit.srv.dynamicProgramming.longestcommonsubsequence;
 
-import java.util.Arrays;
-
-public class LCSTopDown {
+public class LongestCommonSubstring {
     public static void main(String[] args) {
-        String x = "abcdgh";
-        String y = "abedqabcdgabcdghfhr";
+        String x = "zxabcdezy";
+        String y = "yzabcdezx";
+
+        int result = Integer.MIN_VALUE;
 
         int[][] t = new int[x.length() + 1][y.length() + 1];
+
 
         for (int i = 0; i < t.length; i++) {
             t[i][0] = 0;
@@ -21,12 +22,15 @@ public class LCSTopDown {
             for (int j = 1; j < t[i].length; j++) {
                 if (x.charAt(i - 1) == y.charAt(j - 1)) {
                     t[i][j] = 1 + t[i - 1][j - 1];
+                    if (t[i][j] > result) {
+                        result = t[i][j];
+                    }
                 } else {
-                    t[i][j] = Math.max(t[i - 1][j], t[i][j - 1]);
+                    t[i][j] = 0;
                 }
             }
         }
-        System.out.println(Arrays.deepToString(t));
-        System.out.println(t[x.length()][y.length()]);
+
+        System.out.println(result);
     }
 }
