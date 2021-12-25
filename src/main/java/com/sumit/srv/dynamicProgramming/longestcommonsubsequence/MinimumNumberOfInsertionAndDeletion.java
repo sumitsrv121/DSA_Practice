a7757865e3804cd6e9453d9702d81ed63bea271a
sumit <sumitsaurav121@gmail.com>
2021-12-25 22:32:29 +0530

    Minimum number of insertions and deletions

---
+++ src/main/java/com/sumit/srv/dynamicProgramming/longestcommonsubsequence/MinimumNumberOfInsertionAndDeletion.java
@@ -0,0 +1,35 @@
+package com.sumit.srv.dynamicProgramming.longestcommonsubsequence;
+
+public class MinimumNumberOfInsertionAndDeletion {
+    public static void main(String[] args) {
+        String x = "geeksforgeeks";
+        String y = "geeks";
+
+        int[][] dp = new int[x.length() + 1][y.length() + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 1; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int lcs = dp[x.length()][y.length()];
+        System.out.println(lcs);
+
+        // insertion
+        System.out.println("Insertions " + (y.length() - lcs));
+        // deletion
+        System.out.println("Deletions " + (x.length() - lcs));
+    }
+}
