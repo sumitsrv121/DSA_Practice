ab36305778835c41049e0d4222620d18bc8c9bb2
sumit <sumitsaurav121@gmail.com>
2021-12-25 16:26:16 +0530

    LCS top down

---
+++ src/main/java/com/sumit/srv/dynamicProgramming/longestcommonsubsequence/LCSTopDown.java
@@ -0,0 +1,32 @@
+package com.sumit.srv.dynamicProgramming.longestcommonsubsequence;
+
+import java.util.Arrays;
+
+public class LCSTopDown {
+    public static void main(String[] args) {
+        String x = "abcdgh";
+        String y = "abedfhr";
+
+        int[][] t = new int[x.length() + 1][y.length() + 1];
+
+        for (int i = 0; i < t.length; i++) {
+            t[i][0] = 0;
+        }
+
+        for (int i = 1; i < t[0].length; i++) {
+            t[0][i] = 0;
+        }
+
+        for (int i = 1; i < t.length; i++) {
+            for (int j = 1; j < t[i].length; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                } else {
+                    t[i][j] = Math.max(t[i - 1][j], t[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(t));
+        System.out.println(t[x.length()][y.length()]);
+    }
+}
