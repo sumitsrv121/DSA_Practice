e1739cf1f7867529cc8bc5204ac64b4f22ae76b8
sumit <sumitsaurav121@gmail.com>
2021-12-25 11:59:19 +0530

    changed array to local variable than static variable

--- src/main/java/com/sumit/srv/dynamicProgramming/unboundedKnapsack/CoinChangeMaximumNumberOfWaysWithNoTwosTogether.java
+++ src/main/java/com/sumit/srv/dynamicProgramming/unboundedKnapsack/CoinChangeMaximumNumberOfWaysWithNoTwosTogether.java
@@ -3,11 +3,10 @@ package com.sumit.srv.dynamicProgramming.unboundedKnapsack;
 import java.util.Arrays;
 
 public class CoinChangeMaximumNumberOfWaysWithNoTwosTogether {
-    private static long[] dp;
 
     public static void main(String[] args) {
         int sum = 100;
-        dp = new long[sum + 1];
+        long[] dp = new long[sum + 1];
         Arrays.fill(dp, -1);
         dp[0] = 1;
         for (int i = 1; i < dp.length; i++) {
