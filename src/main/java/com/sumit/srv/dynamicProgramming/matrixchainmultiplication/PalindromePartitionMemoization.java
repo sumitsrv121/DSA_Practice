0e5c389eb8b11480e0ae7901772135ddd9ec2823
sumit <sumitsaurav121@gmail.com>
2021-12-27 05:21:55 +0530

    Palindromic partition using memoization

--- src/main/java/com/sumit/srv/dynamicProgramming/matrixchainmultiplication/PalindromePartitionMemoization.java
+++ src/main/java/com/sumit/srv/dynamicProgramming/matrixchainmultiplication/PalindromePartitionMemoization.java
@@ -1,25 +1,37 @@
 package com.sumit.srv.dynamicProgramming.matrixchainmultiplication;
 
-public class PalindromePartitioning {
+import java.util.Arrays;
+
+public class PalindromePartitionMemoization {
+    private static int[][] dp;
+
     public static void main(String[] args) {
         String x = "ababbbabbababa";
 
-        int partitions = findMinimumPartition(x, 0, x.length() - 1);
-        System.out.println(partitions);
+        dp = new int[x.length() + 1][x.length() + 1];
+        for (int[] ints : dp) {
+            Arrays.fill(ints, -1);
+        }
+        int val = findNumberOfPartitions(x, 0, x.length() - 1);
+        System.out.println(val);
     }
 
-    private static int findMinimumPartition(String x, int i, int j) {
+    private static int findNumberOfPartitions(String x, int i, int j) {
         if (i >= j) {
             return 0;
         }
         if (isPalindrome(x, i, j)) {
             return 0;
         }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
         int min = Integer.MAX_VALUE;
-        for (int k = i; k < j; k++) {
-            int temp = findMinimumPartition(x, i, k) + findMinimumPartition(x, k + 1, j) + 1;
+        for (int k = i + 1; k <= j; k++) {
+            int temp = findNumberOfPartitions(x, i, k - 1) + findNumberOfPartitions(x, k, j) + 1;
             if (temp < min) {
                 min = temp;
+                dp[i][j] = min;
             }
         }
         return min;
