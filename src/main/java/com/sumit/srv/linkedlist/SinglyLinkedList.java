fee8cbbe56ea7649f40fb13a241fc2a577492667
sumit <sumitsaurav121@gmail.com>
2022-01-23 16:04:16 +0530

    Reversal of linked list

--- src/main/java/com/sumit/srv/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/sumit/srv/linkedlist/SinglyLinkedList.java
@@ -22,6 +22,15 @@ public class SinglyLinkedList {
         }
     }
 
+    public static void deleteAllElementsOfLinkedList() {
+        Node ptr = HEAD;
+        while (ptr != null) {
+            Node prev = ptr;
+            ptr = ptr.next;
+            prev.next = null;
+        }
+    }
+
     public static Node findMiddleElement() {
         Node slow = HEAD;
         Node fast = HEAD;
@@ -33,6 +42,13 @@ public class SinglyLinkedList {
         return slow;
     }
 
+    public static void printLinkedListInReverseOrder(Node head) {
+        if (head == null) {
+            return;
+        }
+        printLinkedListInReverseOrder(head.next);
+        System.out.print(head.data + "--->");
+    }
 
     public static void removeMiddleElementFromLinkedList() {
         Node slow = HEAD;
@@ -50,6 +66,25 @@ public class SinglyLinkedList {
         }
     }
 
+    public static Node reverseLinkedListIterative(Node head) {
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void printLinkedList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + "--->");
+            head = head.next;
+        }
+    }
+
     public static void main(String[] args) {
         addNodeToLinkedList(new Node(1));
         addNodeToLinkedList(new Node(2));
@@ -64,5 +99,11 @@ public class SinglyLinkedList {
         System.out.println(middleElement.data);
         removeMiddleElementFromLinkedList();
         printLinkedList();
+        System.out.println();
+        printLinkedListInReverseOrder(HEAD);
+        System.out.println();
+        Node HEAD1 = reverseLinkedListIterative(HEAD);
+        printLinkedList(HEAD1);
+        System.out.println();
     }
 }
