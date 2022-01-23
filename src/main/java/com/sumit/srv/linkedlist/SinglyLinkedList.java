5c0d62da5ba35eab7badb2e606c32d64a264745d
sumit <sumitsaurav121@gmail.com>
2022-01-23 10:42:44 +0530

    Implementation of singly linked list

---
+++ src/main/java/com/sumit/srv/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,37 @@
+package com.sumit.srv.linkedlist;
+
+public class SinglyLinkedList {
+    private static Node HEAD = null;
+
+    public static void addNodeToLinkedList(Node newNode) {
+        if (HEAD == null) {
+            HEAD = newNode;
+            return;
+        }
+        Node ptr = HEAD;
+        while (ptr.next != null) {
+            ptr = ptr.next;
+        }
+        ptr.next = newNode;
+    }
+
+    public static void printLinkedList() {
+        Node ptr = HEAD;
+        while (ptr != null) {
+            System.out.print(ptr.data + "--->");
+            ptr = ptr.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        addNodeToLinkedList(new Node(1));
+        addNodeToLinkedList(new Node(2));
+        addNodeToLinkedList(new Node(3));
+        addNodeToLinkedList(new Node(4));
+        addNodeToLinkedList(new Node(5));
+        addNodeToLinkedList(new Node(6));
+
+        printLinkedList();
+        System.out.println();
+    }
+}
